a67799fd3d2bdfd983a864238f5a7e2cc85dbeeb
Brandon Swenson <bmswens@gmail.com>
2020-05-31 00:24:30 -1000

    arg order matters in docker-compose

--- bin/run.sh
+++ bin/run.sh
@@ -4,7 +4,7 @@ dir_path=$(dirname $full_path)
 base_path=$(dirname $dir_path)
 
 if hash docker-compose 2>/dev/null; then
-    docker-compose -f ${base_path}/conf/docker-compose.yml -d up
+    docker-compose -f ${base_path}/conf/docker-compose.yml up -d
 else
     docker run -d --privileged -v /opt/vc:/opt/vc -p 8080:8080 bmswens/mjpg-streamer-raspbian:latest
 fi
