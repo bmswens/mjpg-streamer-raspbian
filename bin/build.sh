7592c1bbd8864adc47fb83629ec7c3b6f938672f
Brandon Swenson <bmswens@gmail.com>
2020-05-30 23:17:59 -1000

    corrected docker image tag

--- bin/build.sh
+++ bin/build.sh
@@ -3,5 +3,5 @@ full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
 base_path=$(dirname $dir_path)
 cd ${base_path}/docker
-docker build -t mjpg-streamer-raspbian .
+docker build -t bmswens/mjpg-streamer-raspbian .
 cd -
